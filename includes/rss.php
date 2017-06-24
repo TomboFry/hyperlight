fc7417d2bf923399198f6952f6eb423bbea40da2
TomboFry <tombofry@hotmail.co.uk>
2017-06-24 23:49:56 +0100

    Improve RSS feed syntax

--- includes/rss.php
+++ includes/rss.php
@@ -3,17 +3,20 @@
 function rss_xml($Blog) {
 	$title = Config::Title;
 	$url = get_base_url() . Config::Root;
-	header("Content-Type: text/xml;");
-	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><rss version=\"2.0\">
-<channel><title>{$title}</title><link>{$url}</link>
-<lastBuildDate>{$Blog->posts[0]->date_datetime()}</lastBuildDate>";
+	$lastBuildDate = gmdate(DATE_RFC2822, $Blog->posts[0]->timestamp);
+	header("Content-Type: application/rss+xml;");
+	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
+<channel><title>{$title}</title><atom:link href=\"{$url}rss\" rel=\"self\" type=\"application/rss+xml\" />
+<description>{$title}</description><link>{$url}</link>
+<lastBuildDate>{$lastBuildDate}</lastBuildDate>";
 	foreach ($Blog->posts as $key => $post) {
-		$pubDate = $post->date_datetime();
+		$pubDate = gmdate(DATE_RFC2822, $post->timestamp);
 		echo "<item>";
 		echo "<title>{$post->title}</title>";
 		echo "<pubDate>{$pubDate}</pubDate>";
 		echo "<link>{$url}post/{$post->slug}</link>";
-		echo "<guid>{$post->slug}</guid>";
+		echo "<guid>{$url}post/{$post->slug}</guid>";
 		if ($post->summary !== "") { echo "<description>{$post->summary}</description>"; }
 		foreach ($post->tags as $key => $category) {
 			echo "<category>{$category}</category>";
