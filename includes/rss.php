06d714a2a112da4eca53c7cf3c8d630637f7ebbc
Tom Gardiner <tom@tombofry.co.uk>
2020-05-10 12:06:06 +0100

    Format RSS XML

--- includes/rss.php
+++ includes/rss.php
@@ -6,12 +6,16 @@ function rss_xml($Blog) {
 	$title = Config::Title;
 	$url = get_base_url() . Config::Root;
 	$lastBuildDate = gmdate(DATE_RFC2822, $Blog->posts[0]->timestamp);
+
 	header("Content-Type: application/rss+xml;");
 	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
-<channel><title>{$title}</title><atom:link href=\"{$url}rss\" rel=\"self\" type=\"application/rss+xml\" />
-<description>{$title}</description><link>{$url}</link>
-<lastBuildDate>{$lastBuildDate}</lastBuildDate>";
+<channel>
+	<title>{$title}</title>
+	<atom:link href=\"{$url}rss\" rel=\"self\" type=\"application/rss+xml\" />
+	<description>{$title}</description>
+	<link>{$url}</link>
+	<lastBuildDate>{$lastBuildDate}</lastBuildDate>";
 	foreach ($Blog->posts as $key => $post) {
 		$pubDate = gmdate(DATE_RFC2822, $post->timestamp);
 		echo "<item>";
