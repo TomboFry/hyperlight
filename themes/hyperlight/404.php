f6ed88637a59ed0f39962a3ec2287eabe58e8902
TomboFry <tom@tombofry.co.uk>
2024-06-21 12:05:24 +0100

    Add site description to footer

--- themes/hyperlight/404.php
+++ themes/hyperlight/404.php
@@ -1,4 +1,4 @@
 <div class="entry">
 	<h2 class="entry-title">404 - Page Not Found</h2>
-	<p class="summary">Sorry guy, but we couldn't find what you were looking for.</p>
+	<p class="summary">Sorry, we couldn't find what you were looking for.</p>
 </div>
